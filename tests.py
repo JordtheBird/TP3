718dd1b256c65ef5253ef84646966b4fd75ffb67
Dominic Labbé <labbe.dom@gmail.com>
2021-07-06 11:12:40 -0400

    Changement de notre version du TP3 pour la nouvelle version d'Honoré

--- tests.py
+++
@@ -1,8 +0,0 @@
-dict = {1:"aawtwtsdghah", 2:"aahgfgsvah", 3:"aaewtwhah", 4:"aahsghah", 5:"aahasgsb sh", 6:"aahahyyr", 7:"aaharrqrqrh", 8:"aaaahah", 9:"aacchah", 10:"aahbbah"}
-
-print(dict.keys())
-
-class Shows:
-    CONSTANTE = 10
-
-print(Shows.CONSTANTE)
